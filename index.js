408dcf6da5110ef69c6941994dcc2c89b48c9d37
steven-guimaraes <stivem.guimaraes@etec.sp.gov.br>
2022-09-11 23:53:19 -0300

    adicionando a pagina de cadastramento
    arrumei tambem o backend e adicionei a imagem na pagina principal, tirei a pasta do client e server, aparentemente node não estava conseguindo pegar essas pastas, provavelmente teve outra maneira, mas tirei mesmo assim

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 8001;
+
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/html/index.html"));
+});
+
+app.listen(port, () => {
+  console.log("servidor nodemon funcionando finalmente");
+});

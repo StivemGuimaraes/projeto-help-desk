93dce778ddc0e13c757986876d6bed6f872f4652
steven-guimaraes <stivem.guimaraes@etec.sp.gov.br>
2022-10-03 22:28:09 -0300

    colocando os aquivos estaticos

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 8001;
+const port = 8008;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+console.log(path.join(__dirname, "/public"));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/html/index.html"));
+  res.sendFile(path.join(__dirname, "/html/index.html"));
+});
+
+app.get("/Cadastro", (req, res) => {
+  res.sendFile(path.join(__dirname, "/html/cadastro.html"));
+});
+
+app.get("/Login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/html/login.html"));
 });
 
 app.listen(port, () => {

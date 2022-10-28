378b1278c6a02b6498d21e223b478159957680a3
stivem <stivem.guimaraes@etec.sp.gov.br>
2022-10-27 22:26:29 -0300

    fazendo o banco de dados

--- db_academy_desk.sql
+++ db_academy_desk.sql
@@ -30,7 +30,7 @@ id int not null primary key auto_increment,
 titulo varchar(90) not null,
 assunto varchar(20) not null,
 nome_cliente varchar(90) not null,
-statusd varchar(20) default Aberto,
+statusd varchar(20) default "Aberto",
 nivel char(1) not null,
 prioridade varchar(8),
 descricao text not null
@@ -46,5 +46,20 @@ add foreign key(id_professor) references professor(matricula);
 
 alter table funcionario
 add foreign key(id_chamado) references chamado(id);
+
+create table chat (
+id_chat int not null auto_increment, 
+id_chamado int,
+matricula_A int,
+matricula_F int,
+mens_professor text not null,
+mens_aluno text not null,
+id_funcionario int,
+mens_func text not null
+);
+alter table chat
+add foreign key(id_chamado) references chamado(id),
+add foreign key(matricula_A) references aluno(matricula),
+add foreign key(matricula_F) references professor(matricula);
 
  

bae7a9a235ab2fe1da42d271a58e26f3e13398e0
stivem-guimaraes <stivem.guimaraes@etec.sp.gov.br>
2023-06-11 01:02:32 -0300

    teste1

--- conexao.js
+++ conexao.js
@@ -1,6 +1,8 @@
 const con = () => {
+  if (global.con && global.con.state !== "disconnected") return global.con;
+
   try {
-    const mysql = require("mysql2");
+    const mysql = require("mysql2/promise");
     const con = mysql.createPool({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
@@ -11,14 +13,15 @@ const con = () => {
       connectionLimit: 30,
       queueLimit: 0,
     });
+    global.con = con;
     return con;
   } catch (error) {
-    console.log("algo deu erro na conexão com banco de dados", error);
+    console.log("algo deu erro na conexão com banco de dados:", error);
     console.log(process.env.DB_HOST);
-    console.log(process.env.DB_USER);
-    console.log(process.env.DB_PORT);
     console.log(process.env.DB_DATABASE);
+    console.log(process.env.DB_USER);
     console.log(process.env.DB_PASSWORD);
+    console.log(process.env.DB_PORT);
   }
 };
 module.exports = { con };

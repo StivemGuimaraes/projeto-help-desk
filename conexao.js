bb7a386cc2e316896e2472958f151ef9782731aa
stivem-guimaraes <stivem.guimaraes@etec.sp.gov.br>
2023-06-11 01:28:19 -0300

    teste feito com sucesso

--- conexao.js
+++ conexao.js
@@ -4,11 +4,10 @@ const con = () => {
   try {
     const mysql = require("mysql2/promise");
     const con = mysql.createPool({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      port: process.env.DB_PORT,
-      database: process.env.DB_DATABASE,
-      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST | "localhost",
+      user: process.env.DB_USER | "root",
+      database: process.env.DB_DATABASE | "academy_desk",
+      password: process.env.DB_PASSWORD | "Mysqlstivem123",
       waitForConnections: true,
       connectionLimit: 30,
       queueLimit: 0,
@@ -17,11 +16,6 @@ const con = () => {
     return con;
   } catch (error) {
     console.log("algo deu erro na conexão com banco de dados:", error);
-    console.log(process.env.DB_HOST);
-    console.log(process.env.DB_DATABASE);
-    console.log(process.env.DB_USER);
-    console.log(process.env.DB_PASSWORD);
-    console.log(process.env.DB_PORT);
   }
 };
 module.exports = { con };

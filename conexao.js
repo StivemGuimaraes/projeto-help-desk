2ec55a8a182b5adafa2d899e1701dad46d2a59f2
stivem-guimaraes <stivem.guimaraes@etec.sp.gov.br>
2023-06-11 14:13:06 -0300

    teste1

--- conexao.js
+++ conexao.js
@@ -4,10 +4,10 @@ const con = () => {
   try {
     const mysql = require("mysql2/promise");
     const con = mysql.createPool({
-      host: process.env.DB_HOST | "localhost",
-      user: process.env.DB_USER | "root",
-      database: process.env.DB_DATABASE | "academy_desk",
-      password: process.env.DB_PASSWORD | "Mysqlstivem123",
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
       waitForConnections: true,
       connectionLimit: 30,
       queueLimit: 0,

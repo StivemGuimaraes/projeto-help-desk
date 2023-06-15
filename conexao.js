a6337cd2d44a3735866f7008f60511a08d44cece
willian braga <willian.sousa21@etec.sp.gov.br>
2023-06-14 21:35:46 -0300

    teste produção 6

--- conexao.js
+++ conexao.js
@@ -4,10 +4,10 @@ const con = () => {
   try {
     const mysql = require("mysql2/promise");
     const con = mysql.createPool({
-      host: "localhost",
-      user: "root",
-      database: "academy_desk",
-      password: "",
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
       waitForConnections: true,
       connectionLimit: 30,
       queueLimit: 0,

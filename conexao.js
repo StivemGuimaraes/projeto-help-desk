a53f272deed7dcf839b935e6339da4b07a9fad16
stivem-guimaraes <stivem.guimaraes@etec.sp.gov.br>
2023-06-17 23:40:36 -0300

    teste de produçao com uma nova hospedagem de banco de dados

--- conexao.js
+++ conexao.js
@@ -4,10 +4,10 @@ const con = () => {
   try {
     const mysql = require("mysql2/promise");
     const con = mysql.createPool({
-      host: "localhost",
-      user: "root",
-      database: "academy_desk",
-      password: "Mysqlstivem123",
+      host: process.env.HOST,
+      user: process.env.USER,
+      database: process.env.DATABASE,
+      password: process.env.PASSWORD,
       waitForConnections: true,
       connectionLimit: 30,
       queueLimit: 0,
